be6daf3a776e6f3f42c857cf561fd0b0a1ee0116
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-05-26 09:02:30 -0700

    Add plugin structure to register blocks and placeholders for new blocks.

--- includes/class-cagov-design-system.php
+++ includes/class-cagov-design-system.php
@@ -20,6 +20,7 @@ class CAGOVDesignSystem {
     private function __construct(){
         // Load All Block Files
         $this->load_block_dependencies();
+        $this->load_web_component_dependencies();
     }
 
     private function load_block_dependencies(){
@@ -27,8 +28,38 @@ class CAGOVDesignSystem {
         // ROADMAP: Eventually, when there are more dependencies and installers would like to control which blocks are enabled, we can alter this function & create an admin page that will let administrators enable and disable blocks.
         // For now, all of the blocks will load.
         // New blocks should be registered alphabetically.
+        require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/alert/plugin.php';
         require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/card/plugin.php';
         require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/card-grid/plugin.php';
+        require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/content-navigation/plugin.php';
         require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/hero/plugin.php';
+        require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/news-list/plugin.php';
+        // require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/post-event/plugin.php';
+        // require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/post-announcement/plugin.php';
+        // require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/post-press-release/plugin.php';        
+        // require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/recent-news/plugin.php';
+        // require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/blocks/twitter-embed/plugin.php';
+
+    }
+
+    private function load_web_component_dependencies(){
+        // Global dependencies
+        wp_enqueue_script(
+            'moment'
+        );
+
+        // Custom web components javascript and css
+        wp_enqueue_script(
+            'california-design-system-news-list-web-component',
+            plugins_url( '/blocks/news-list/web-component.js', dirname( __FILE__ ) ),
+            array( ),
+            // Version?
+        );
+        wp_enqueue_style(
+            'california-design-system-news-list',
+            plugins_url( '/blocks/news-list/style.css', dirname( __FILE__ ) ),
+            array( )
+            
+        );
     }
 }
