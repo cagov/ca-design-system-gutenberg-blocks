d0bf400ef1f108a645a78466285181a86190f93a
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-06-02 16:52:19 -0700

    starter files for CAGov Accordion

---
+++ blocks/accordion/block.js
@@ -0,0 +1,136 @@
+/**
+ * CAGov Accordion
+ */
+
+(function (blocks, blockEditor, i18n, element, components, _) {
+
+  var __ = i18n.__;
+  var el = element.createElement;
+  var RichText = blockEditor.RichText;
+  var PlainText = blockEditor.PlainText;
+  var TextControl = components.TextControl;
+
+  blocks.registerBlockType("cagov/accordion", {
+    title: __("CAGov: Accordion", "cagov-design-system"),
+    icon: "universal-access-alt",
+    anchor: "layout",
+    attributes: {
+      title: {
+        type: "array",
+        source: "children",
+        selector: "h3",
+        default: "Title",
+      },
+      content: {
+        type: "array",
+        source: "children",
+        selector: "p.content",
+      },
+      anchor: {
+        type: "string",
+        source: "attribute",
+        selector: "p.accordion",
+        default: "#anchor",
+      },
+      isOpen: {
+        type: "string",
+        source: "html",
+        selector: "p.is-open",
+        default: 'Open',
+      },
+    },
+    example: {
+      attributes: {
+        title: __("Accordion title", "cagov-design-system"),
+        content: __("Accordion content", "cagov-design-system"),
+        anchor: __("Link Text", "cagov-design-system"),
+        isOpen: __("Open", "cagov-design-system"),
+      },
+    },
+    edit: function (props) {
+      var attributes = props.attributes;
+      return el(
+        "div",
+        {
+          className: "cagov-accordion cagov-stack",
+        },
+        el(
+          "div",
+          {},
+          el(RichText, {
+            tagName: "h3",
+            inline: false,
+            placeholder: __("Accordion title", "cagov-design-system"),
+            value: attributes.title,
+            onChange: function (value) {
+              props.setAttributes({ title: value });
+            },
+          }),
+          el(RichText, {
+            tagName: "p.content",
+            inline: false,
+            placeholder: __("Accordion content", "cagov-design-system"),
+            value: attributes.content,
+            onChange: function (value) {
+              props.setAttributes({ content: value });
+            },
+          }),
+          el(RichText, {
+            tagName: "p.anchor",
+            inline: false,
+            placeholder: __("Accordion anchor", "cagov-design-system"),
+            value: attributes.anchor,
+            onChange: function (value) {
+              props.setAttributes({ anchor: value });
+            },
+          }),
+          el(RichText, {
+            tagName: "p.is-open",
+            inline: false,
+            placeholder: __("Accordion is open t/f", "cagov-design-system"),
+            value: attributes.isOpen,
+            onChange: function (value) {
+              props.setAttributes({ isOpen: value });
+            },
+          }),
+         
+        )
+      );
+    },
+    // https://developer.wordpress.org/block-editor/reference-guides/block-api/block-edit-save/
+    save: function (props) {
+      var attributes = props.attributes;
+      return el(
+        "div",
+        { className: "cagov-accordion cagov-stack" },
+        el(
+          "div",
+          {},
+          el(RichText.Content, {
+            tagName: "h3",
+            value: attributes.title,
+          }),
+          el(RichText.Content, {
+            tagName: "p.content",
+            value: attributes.content,
+          }),
+          el(RichText.Content, {
+            tagName: "p.anchor",
+            value: attributes.isOpen,
+          }),
+          el(RichText.Content, {
+            tagName: "p.is-open",
+            value: attributes.isOpen,
+          }),
+        )
+      );
+    },
+  });
+})(
+  window.wp.blocks,
+  window.wp.blockEditor,
+  window.wp.i18n,
+  window.wp.element,
+  window.wp.components,
+  window._,
+);
---
+++ blocks/accordion/plugin.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ * Plugin Name: Accordion
+ * Plugin URI: TBD
+ * Description: TBD
+ * Version: 1.1.0
+ * Author: California Office of Digital Innovation
+ * @package cagov-design-system
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Load all translations for our plugin from the MO file.
+ */
+add_action( 'init', 'cagov_design_system_gutenberg_block_accordion' );
+
+function cagov_design_system_gutenberg_block_accordion() {
+	load_plugin_textdomain( 'cagov-design-system', false, basename( __DIR__ ) . '/languages' );
+}
+
+/**
+ * Registers all block assets so that they can be enqueued through Gutenberg in
+ * the corresponding context.
+ *
+ * Passes translations to JavaScript.
+ */
+function cagov_design_system_register_accordion() {
+
+	if ( ! function_exists( 'register_block_type' ) ) {
+		// Gutenberg is not active.
+		return;
+	}
+
+	// Register custom web component
+	wp_register_script(
+		'california-design-system-accordion-web-component',
+		plugins_url( 'web-component.js', __FILE__ ),
+		array( ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'web-component.js' ),
+	);
+
+	wp_register_script(
+		'california-design-system-accordion',
+		plugins_url( 'block.js', __FILE__ ),
+		array( 'wp-blocks', 'wp-i18n', 'wp-element', 'wp-editor', 'underscore', 'moment', 'california-design-system-accordion-web-component' ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'block.js' ),
+	);
+
+	wp_register_style(
+		'california-design-system-accordion',
+		plugins_url( 'style.css', __FILE__ ),
+		array( ),
+		filemtime( plugin_dir_path( __FILE__ ) . 'style.css' )
+	);
+
+	register_block_type( 'cagov/accordion', array(
+		'style' => 'cagov-accordion',
+		'editor_script' => 'california-design-system-accordion',
+	) );
+
+}
+add_action( 'init', 'cagov_design_system_register_accordion' );
