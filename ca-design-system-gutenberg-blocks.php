f0a1985de03dc915ae2b73e190e223e412928bd7
Danny Guzman <guzmanjd86@gmail.com>
2021-06-03 15:37:22 -0700

    Renamed plugin main file to match WordPress Best Practices Folder Structure Added add action for admin_init

--- ca-design-system-gutenberg-blocks.php
+++ ca-design-system-gutenberg-blocks.php
@@ -20,9 +20,24 @@ define('CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH', plugin_dir_path(__FILE__));
 define('CAGOV_DESIGN_SYSTEM_ADMIN_URL', plugin_dir_url(__FILE__));
 define('CAGOV_DESIGN_SYSTEM_FILE', __FILE__);
 
+add_action( 'admin_init', 'cagov_design_system_admin_init' );
+
+/**
+ * Admin Init
+ *
+ * Triggered before any other hook when a user accesses the admin area.
+ * Note, this does not just run on user-facing admin screens.
+ * It runs on admin-ajax.php and admin-post.php as well.
+ *
+ * @link https://codex.wordpress.org/Plugin_API/Action_Reference/admin_init
+ * @return void
+ */
+function cagov_design_system_admin_init(){
+    include_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/core/class-cagov-design-system-plugin-update.php';
+}
+
 if( ! class_exists('CAGOVDesignSystem') ) {
     require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/includes/class-cagov-design-system.php';
-    //require_once CAGOV_DESIGN_SYSTEM_BLOCKS_DIR_PATH . '/core/class-cagov-design-system-plugin-update.php';
 }
 
 CAGOVDesignSystem::get_instance();
