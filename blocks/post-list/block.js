9ae495ff96afcfb57d62840ed05b0263306f4461
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-06-16 20:14:39 -0700

    Figure out how to add component controls like toggle button and check box for block settings - even though they will belong in the side panel

--- blocks/post-list/block.js
+++ blocks/post-list/block.js
@@ -1,21 +1,24 @@
 /**
  * CAGov Post list
  * List of recent posts. Appears on the homepage. Allows people to see the most recent announcements with the ""Announcement"" tag. Includes title, hyperlink to full announcement, date, and a view all link to see longer list.
- * 
+ *
  */
-
 
 // https://developer.wordpress.org/block-editor/how-to-guides/block-tutorial/creating-dynamic-blocks/
 
- (function (blocks, blockEditor, i18n, element, components, _, moment) {
+(function (blocks, editor, i18n, element, components, _, moment) {
   var __ = i18n.__;
   var el = element.createElement;
   // https://github.com/WordPress/gutenberg/blob/HEAD/packages/block-editor/src/components/rich-text/README.md
 
   //http://wordpress.test:8888/wp-json/wp/v2/tags?per_page=10&orderby=count&order=desc&_fields=id%2Cname%2Ccount&context=edit&_locale=user".
-  var RichText = blockEditor.RichText;
+  var RichText = editor.RichText;
+  var InnerBlocks = editor.InnerBlocks;
+  var CheckboxControl = components.CheckboxControl;
   var TextControl = components.TextControl;
-  const BlockControls = editor.BlockControls;
+  var ToggleControl = components.ToggleControl;
+
+  // Ref: https://developer.wordpress.org/block-editor/reference-guides/components/checkbox-control/
 
   let siteUrl = window.location.origin;
 
@@ -23,7 +26,10 @@
     title: __("Post list", "ca-design-system"),
     icon: "universal-access-alt",
     category: "ca-design-system-utilities",
-    description: __("List of recent announcements. Appears on the homepage. Allows people to see the most recent announcements with the \"Announcement\" tag. Includes title, hyperlink to full announcement, date, and a view all link to see longer list.", "ca-design-system"),
+    description: __(
+      'List of recent announcements. Appears on the homepage. Allows people to see the most recent announcements with the "Announcement" tag. Includes title, hyperlink to full announcement, date, and a view all link to see longer list.',
+      "ca-design-system"
+    ),
     attributes: {
       title: {
         type: "string",
@@ -31,25 +37,26 @@
       },
       category: {
         type: "string",
-        // default: "announcements,press-releases",
+        default: "announcements,press-releases",
       },
       readMore: {
         type: "string",
       },
       order: {
         type: "string",
-        // default: "desc",
+        default: "desc",
       },
       count: {
         type: "string",
-        // default: "10",
+        default: "10",
       },
       endpoint: {
         type: "string",
-        // default: `${siteUrl}/wp-json/wp/v2`,
+        default: `${siteUrl}/wp-json/wp/v2`,
       },
       showExcerpt: {
         type: "string",
+        default: "true"
       },
     },
     example: {
@@ -90,22 +97,39 @@
             "data-show-excerpt": attributes.showExcerpt,
           }),
           // Visual display of endpoint
-          el(RichText, {
-            tagName: "div",
-            className: "read-more",
-            inline: false,
-            placeholder: __("Link to post page", "ca-design-system"),
-            value: attributes.readMore,
-            onChange: function (value) {
-              props.setAttributes({ readMore: value });
+          // el(RichText, {
+          //   tagName: "div",
+          //   className: "read-more",
+          //   inline: false,
+          //   placeholder: __("Link to post page", "ca-design-system"),
+          //   value: attributes.readMore,
+          //   onChange: function (value) {
+          //     props.setAttributes({ readMore: value });
+          //   },
+          // }),
+          el(
+            "div",
+            {
+              tagName: "div",
+              className: "read-more",
+              inline: false,
+              placeholder: __("Link to post page", "ca-design-system"),
             },
-          }),
+            el(InnerBlocks, {
+            //   value: attributes.readMore,
+              allowedBlocks: ["core/paragraph", "core/button"],
+              onChange: function (value) {
+                props.setAttributes({ readMore: value });
+              },
+            })
+          ),
           el(
             "div",
             { className: "edit" },
-            el(RichText, {
+            el(TextControl, {
               label: "Change post category",
               tagName: "div",
+              help: "Comma separated list of category slugs", // @TODO Will change to select box
               className: "post-list-category",
               inline: false,
               placeholder: __("Category", "ca-design-system"),
@@ -114,43 +138,53 @@
                 props.setAttributes({ category: value });
               },
             }),
-            el(RichText, {
+            el(TextControl, {
               label: "Number of items to display",
               tagName: "div",
+              
               className: "post-list-count",
               value: attributes.count,
               inline: false,
-              placeholder: __("Number items to display (recommend 10)", "ca-design-system"),
+              placeholder: __(
+                "Number items to display (recommend 10)",
+                "ca-design-system"
+              ),
               onChange: function (value) {
-                console.log('value count', value);
+                console.log("value count", value);
                 props.setAttributes({ count: value });
               },
             }),
-            el(RichText, {
-              // label: "Order",
+            el(ToggleControl, {
+              label: "Order of posts",
               tagName: "div", // Checkbox desc/asc
               className: "post-list-order",
               inline: false,
-              // type: "hidden",
-              placeholder: __("Recent first", "ca-design-system"),
-              value: attributes.order,
+              help: attributes.order === "asc" ?  "Oldest first" : "Newest posts first",
+              placeholder: __("Newest posts first", "ca-design-system"),
+              value: attributes.order === "desc" || "" ? true : false,
               onChange: function (value) {
-                props.setAttributes({ order: value });
+                props.setAttributes({ 
+                  order: value === true ? "desc" : "asc"
+                });
               },
             }),
-            el(RichText, {
-              // label: "Endpoint",
+            el(CheckboxControl, {
+              label: "Show excerpt",
               tagName: "div", // select box + enter data
               className: "post-list-excerpt",
+              help: "Display the excerpt",
               inline: false,
               placeholder: __("Show excerpt", "ca-design-system"),
+              checked:  attributes.showExcerpt === "true" ? true : false,
               value: attributes.showExcerpt,
               onChange: function (value) {
-                props.setAttributes({ showExcerpt: value });
+                props.setAttributes({ 
+                  showExcerpt: value === true ? "true" : "false" 
+                });
               },
             }),
-            el(RichText, {
-              // label: "Endpoint",
+            el(TextControl, {
+              label: "Endpoint",
               tagName: "div", // select box + enter data
               className: "post-list-endpoint",
               inline: false,
