f1d7de813256f4c5f5c46155dffc766cf7e4049a
AaronHans <aaronhans@aaronhansmacpro.gateway.sonic.net>
2021-06-17 19:01:59 -0700

    update path

--- src/client/insert-script.js
+++ src/client/insert-script.js
@@ -1,4 +1,4 @@
 let newScript = document.createElement("script");
 newScript.type="module";
-newScript.src = "https://files.covid19.ca.gov/js/components/accordion/index.js";
+newScript.src = "https://files.covid19.ca.gov/js/components/accordion/v1/index.js";
 document.querySelector('head').appendChild(newScript);
