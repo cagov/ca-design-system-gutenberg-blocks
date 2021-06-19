761549863e500669eca2e216535b8d4f2cf1972d
AaronHans <aaronhans@aaronhansmacpro.gateway.sonic.net>
2021-06-18 17:03:43 -0700

    revise comment

--- src/client/insert-script.js
+++ src/client/insert-script.js
@@ -1,4 +1,4 @@
-// this snippet is inserted into the custom js section of the WordPress CAWeb theme in order to deliver the bundle of client side web components
+// this snippet is inserted into the custom js section of the WordPress CAWeb theme in order to deliver the bundle of client side web components. This bundle is created in the github.com/cagov/cannabis.ca.gov repository
 let newScript = document.createElement("script");
 newScript.type="module";
 newScript.src = "https://cagov.github.io/cannabis.ca.gov/src/js/index.min.js";
