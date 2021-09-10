ffff32ae34e1fba81de50ab00423ed7467246140
chachasikes <149436+chachasikes@users.noreply.github.com>
2021-09-10 11:12:39 -0700

    update plugin documentation

--- blocks/event-detail/plugin.php
+++ blocks/event-detail/plugin.php
@@ -44,7 +44,7 @@ function cagov_register_event_detail()
     // include_once plugin_dir_path(__FILE__) .  '/meta.php';
     // cagov_event_detail_meta_init(); // Initialize meta content & backend field support.
 
-    wp_enqueue_script( 'wp-api' ); // Make the wp-api available directly
+    wp_enqueue_script('wp-api'); // Make the wp-api available directly to the plugin
     // wp_enqueue_script( 'my_script', 'path/to/my/script', array( 'wp-api' ) ); (Alt: dependency)
 
 
@@ -55,24 +55,28 @@ function cagov_register_event_detail()
         'render_callback' => 'cagov_event_detail_dynamic_render_callback'
     ));
 
-    add_action( 'init', 'cagov_event_detail_register_settings' );
+    add_action('init', 'cagov_event_detail_register_settings');
 
     register_setting(
-		'cagov_event_detail_settings',
-		'cagov_event_detail_example_text',
-		[
-			'default'       => '',
-			'show_in_rest'  => true,
-			'type'          => 'string',
-		]
-	);
+        'cagov_event_detail_settings',
+        'cagov_event_detail_example_text',
+        [
+            'default'       => '',
+            'show_in_rest'  => true,
+            'type'          => 'string',
+        ]
+    );
 }
-
+/**
+ * Render design system markup using the block attributes and block content.
+ */
 function cagov_event_detail_dynamic_render_callback($block_attributes, $content)
 {
-    $string_date_time = "Date &amp; time"; // @TODO Storing as variables these need a string registry.
+    $string_date_time = "Date &amp; time"; // @TODO Storing as variables: these will need a string registry.
     $string_location = "Location";
     $string_cost = "Cost";
+
+    // Get the attributes from the Gutenberg block variables.
     $title = isset($block_attributes["title"]) ? $block_attributes["title"] : "Event Details";
     $startDateTimeUTC = isset($block_attributes["startDateTimeUTC"]) ? $block_attributes["startDateTimeUTC"] : "";
     $endDateTimeUTC = isset($block_attributes["endDateTimeUTC"]) ? $block_attributes["endDateTimeUTC"] : "";
@@ -85,24 +89,31 @@ function cagov_event_detail_dynamic_render_callback($block_attributes, $content)
     $location = isset($block_attributes["location"]) ? $block_attributes["location"] : "";
     $cost = isset($block_attributes["cost"]) ? $block_attributes["cost"] : "";
 
-    // Testing API integration
-    // $example_text = get_option( 'cagov_event_detail_example_text' );
-    // '<p class="cagov-event-detail-example">' . $example_text . '</p>' .
-
     // @TODO The text string labels don't belong in this code & need a translated string registry. 
     return '<div class="wp-block-ca-design-system-event-detail-block cagov-event-detail-block cagov-stack">
         <div>
             <h3>' . $title . '</h3>
             <div class="wp-block-ca-design-system-event-detail cagov-event-detail cagov-stack">' .
-            cagov_event_detail_get_date_time_block($string_date_time, $startDate, $endDate, $startTime, $endTime, $localTimezoneLabel) .
-            cagov_event_detail_get_location_block($string_location, $location) .
-            cagov_event_detail_get_cost_block($string_cost, $cost) .
-            cagov_event_detail_get_more_info_block($block_attributes, $content) .
-        '</div>' .
+                cagov_event_detail_get_date_time_block($string_date_time, $startDate, $endDate, $startTime, $endTime, $localTimezoneLabel) .
+                cagov_event_detail_get_location_block($string_location, $location) .
+                cagov_event_detail_get_cost_block($string_cost, $cost) .
+                cagov_event_detail_get_more_info_block($block_attributes, $content) .
+            '</div>' .
         '</div>
     </div>';
 }
 
+/**
+ * Undocumented function
+ *
+ * @param [type] $string_date_time UTC
+ * @param [type] $startDate local timezone extracted display start date
+ * @param [type] $endDate local timezone extracted display end date
+ * @param [type] $startTime local timezone extracted display start time
+ * @param [type] $endTime local timezone extracted display end time
+ * @param [type] $localTimezoneLabel local timezone label
+ * @return void
+ */
 function cagov_event_detail_get_date_time_block($string_date_time, $startDate, $endDate, $startTime, $endTime, $localTimezoneLabel)
 {
     $block_date = "";
@@ -138,9 +149,17 @@ function cagov_event_detail_get_date_time_block($string_date_time, $startDate, $
         '</div>';
 }
 
-function cagov_event_detail_get_location_block($string_location, $location) {
+/**
+ * Location. The location content is currently free text, but future locations can integrate with schema.org data if we make addresses and webinar links storable as fielded content.
+ *
+ * @param [type] $string_location
+ * @param [type] $location
+ * @return void
+ */
+function cagov_event_detail_get_location_block($string_location, $location)
+{
     if ("" !== $location) {
-    return  '<div class="detail-section">
+        return  '<div class="detail-section">
         <h4>' . $string_location . '</h4>
         <div class="location field-data">' . $location . '</div>
     </div>';
@@ -148,7 +167,11 @@ function cagov_event_detail_get_location_block($string_location, $location) {
     return "";
 }
 
-function cagov_event_detail_get_cost_block($string_cost, $cost) {
+/**
+ * Event Cost
+ */
+function cagov_event_detail_get_cost_block($string_cost, $cost)
+{
     if ("" !== $cost) {
         return  '<div class="detail-section">
             <h4>' . $string_cost . '</h4>
@@ -158,14 +181,22 @@ function cagov_event_detail_get_cost_block($string_cost, $cost) {
     return "";
 }
 
-function cagov_event_detail_get_more_info_block($block_attributes, $content) {
-    $body = isset( $block_attributes['body'] ) ? $block_attributes['body'] : '';
-    $inner_blocks = do_blocks( $content );
-    
+/**
+ * Get inner block content of this component and append it to the bottom of the event UI
+ *
+ * @param [type] $block_attributes
+ * @param [type] $content
+ * @return void
+ */
+function cagov_event_detail_get_more_info_block($block_attributes, $content)
+{
+    $body = isset($block_attributes['body']) ? $block_attributes['body'] : '';
+    $inner_blocks = do_blocks($content);
+
     if ("" !== $inner_blocks) {
         return  '<div class="detail-section-more-info">
         <div class="more-info field-data">' . $inner_blocks . '</div>
-        </div>' ;
+        </div>';
     }
     return "";
 }
