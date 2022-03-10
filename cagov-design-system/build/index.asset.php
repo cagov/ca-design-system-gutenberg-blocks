593f4647efb8fac5a3634bf0f9f21d0d188d1a61
Aaron Hans <aaronhans@gmail.com>
2022-03-09 16:45:26 -0800

    use latest accordion v2.0.1, rebuild

--- cagov-design-system/build/index.asset.php
+++ cagov-design-system/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-polyfill'), 'version' => '331bf52db18425b502d8682749d53620');
+<?php return array('dependencies' => array('wp-polyfill'), 'version' => 'dfa42be736fa62cd05692eb4fabee7bc');
